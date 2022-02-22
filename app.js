dea926d3389b6277eac971b3ff3690c1dbce7bc6
ASARABIAS <95699189+ASARABIAS@users.noreply.github.com>
2022-02-22 18:51:10 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('public'));
+//app.use('/static', express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('./views/home.html'));
+});
+
+app.post('/', (req, res) => {
+    res.sendFile(path.resolve('./views/home.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve('./views/register.html'));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve('./views/login.html'));
+});
+
+
+app.listen(3000, () => console.log("Servidor Corriendo"));
